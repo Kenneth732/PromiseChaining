773d0e0117e9ffad343f78cb48b58c43eb43f5f4
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-19 15:35:59 +0300

    retur... pro

--- script.js
+++ script.js
@@ -10,8 +10,64 @@ p.then((result) => {
 }).then((result) =>{
     console.log(result)
     return result * 3;
+}).then((result) => {
+    console.log(result)
+    return result * 4;
 });
 
 // Output
 // 10
 // 20
+// 60
+
+// Multiple handlers for a promise
+
+let j = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10)
+    }, 3 * 1000);
+});
+
+j.then((result) => {
+    console.log(result)
+    return result * 2; // 10
+})
+
+
+j.then((result) => {
+    console.log(result)
+    return result * 3; // 10
+});
+
+j.then((result) => {
+    console.log(result)
+    return result * 4; // 10
+});
+
+// Returning a Promise
+
+let k = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10);
+    }, 3 * 100);
+});
+
+k.then((result) => {
+    console.log(result);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(result * 2);
+        }, 3 * 1000);
+    });
+}).then((result) => {
+    console.log(result);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(result * 3);
+        }, 3 * 1000);
+    });
+}).then(result => console.log(result));
+// Output
+// 10
+// 20
+// 60
